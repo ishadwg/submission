6443395be27be8d3ca5b119ddc152d32ed57144d
ishadwg <ishadwg@gmail.com>
2023-08-14 14:03:19 +0700

    anon component

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    // return view('home');
+    return view('home')->with('username', 'Isha');
 });
 Route::get('/form', function () {
     return view('form');
