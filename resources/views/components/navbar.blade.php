8346aad4fea1336edbc65cbce776e2fc5150f6f4
ishadwg <ishadwg@gmail.com>
2023-08-15 16:50:33 +0700

    Update image logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,7 +2,7 @@
 <nav class="bg-gray-500 p-4 shadow-lg">
     <div class="container mx-auto flex justify-between items-center">
         <a href="#" class="text-white font-semibold text-lg">
-            <img src="https://www.krakatausteel.com/images/reverselogo.png" class="w-20" alt="Home" />
+            <img src="https://www.krakatausteel.com/images/reverselogo.png" class="w-64" alt="Home" />
         </a>
         <ul class="flex space-x-4">
             <li><a href="/" class="text-white hover:text-gray-400">Hi, Username</a></li>
