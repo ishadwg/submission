50ded96b984c8e350d1c33d81dd55d185dc3cfa4
ishadwg <ishadwg@gmail.com>
2023-08-15 16:48:36 +0700

    Add image logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,9 @@
 <!-- Navbar -->
 <nav class="bg-gray-500 p-4 shadow-lg">
     <div class="container mx-auto flex justify-between items-center">
-        <a href="#" class="text-white font-semibold text-lg">Submission</a>
+        <a href="#" class="text-white font-semibold text-lg">
+            <img src="https://www.krakatausteel.com/images/reverselogo.png" class="w-20" alt="Home" />
+        </a>
         <ul class="flex space-x-4">
             <li><a href="/" class="text-white hover:text-gray-400">Hi, Username</a></li>
             <li><a href="/" class="text-white hover:text-gray-400">Home</a></li>
