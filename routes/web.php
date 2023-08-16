eb08e6d893185424a8a86acd98ea33d1b06b0624
ishadwg <ishadwg@gmail.com>
2023-08-16 11:38:01 +0700

    create submission

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\SubmissionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,6 +25,9 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 Route::get('login', [SessionController::class, 'create'])->middleware('guest')->name('login');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
+
+Route::get('submissions/create', [SubmissionController::class, 'create']);
+Route::post('submissions', [SubmissionController::class, 'store']);
 
 Route::get('/form', function () {
     return view('form');
